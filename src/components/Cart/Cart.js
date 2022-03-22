09bf1c1b72614214c67c8d9aaac1eedf8ee13b6e
SHOHAG1611 <mdshohag16112000@gmail.com>
2022-03-22 23:48:34 +0600

    total money added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
+    const { cart } = props;
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
     return (
-        <div>
-            <h1>This is card Sumary</h1>
-            <p>Add Product For Buy:{props.cart.length}</p>
+        <div className='cartStyle'>
+            <h4>This is card Sumary</h4>
+            <p>Selected Product: {cart.length}</p>
+            <p>Total Price: ${total} </p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: {tax}</p>
+            <p>Grand Total: </p>
         </div>
     );
 };
